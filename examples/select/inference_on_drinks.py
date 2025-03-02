c4396c18132de4a0f93b40cd88c242d3e717aaf3
Tyler Liu <liu3450@purdue.edu>
2025-03-02 14:12:17 -0500

    text2text-gen for local model

--- examples/select/inference_on_drinks.py
+++ examples/select/inference_on_drinks.py
@@ -2,12 +2,29 @@ import duckdb
 import sys, os
 sys.path.insert(0, os.path.abspath('.'))
 from udf import *
+from timer import timing
 
-# example use prompt/model specified
-query = r"""
-    SELECT drink, llm_model('ill provide you with a drink and a price in dollars, tell me in 1 word in the drink is cheap, average, or expensive', 'mistralai/Mistral-7B-Instruct-v0.3', drink, price)
-    FROM read_csv('./datasets/drinks.csv')
-"""
-res = duckdb.sql(query).fetchall()
-print(res)
+@timing
+def execute_query():
+    query = r"""
+        SELECT drink, llm_task_local('ill provide you with a drink and a price in dollars, tell me in 1 word in the drink is cheap, average, or expensive', 'text2text-generation', drink, price)
+        FROM read_csv('./datasets/drinks.csv')
+    """
+    res = duckdb.sql(query).fetchall()
+    return res
+
+@timing
+def execute_query_batched():
+    query = r"""
+        SELECT drink, llm_task_batch_local('ill provide you with a drink and a price in dollars, tell me in 1 word in the drink is cheap, average, or expensive', 'text2text-generation', drink, price)
+        FROM read_csv('./datasets/drinks.csv')
+    """
+    res = duckdb.sql(query).fetchall()
+    return res
+
+print(execute_query())
+print(execute_query_batched())
+
+
+
 # res: [('mocha', '.00\naverage'), ('flat white', '.50, average'), ('vanilla latte', '.50\n\nexpensive')]
--- udf/udfs_local.py
+++ udf/udfs_local.py
@@ -11,6 +11,17 @@ def llm_task_local(prompt, task, /, *attributes):
     if task == "summarization":
         summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
         return summarizer(attributes[0], max_length=200, min_length=30, do_sample=False)[0]['summary_text']
+    elif task == "text2text-generation":
+        generator = pipeline("text2text-generation", model="google/flan-t5-small")
+
+        # format prompt
+        llm_input = prompt + ": "
+        for i in range(n_attrs):
+            llm_input += str(attributes[i])
+            if i != n_attrs - 1:
+                llm_input += ", "
+        
+        return generator(llm_input, max_new_tokens=30, do_sample=False)[0]['generated_text']
     else:
         print(task, 'is unsupported')
 
@@ -24,6 +35,23 @@ def llm_task_batch_local(prompt, task, /, *attributes):
         summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
         res = summarizer(attributes[0].to_pylist(), max_length=200, min_length=30, do_sample=False)
         pa_arr = pa.array([res[i]['summary_text'] for i in range(len(attributes[0]))])
+        return pa_arr
+    elif task == "text2text-generation":
+        generator = pipeline("text2text-generation", model="google/flan-t5-small")
+
+        # format prompts
+        llm_input_vector = []
+        prompt = prompt.to_pylist()[0]
+        for row_idx in range(len(attributes[0])):
+            llm_input = prompt + ": "
+            for attr_idx in range(n_attrs):
+                llm_input += str(attributes[attr_idx][row_idx])
+                if attr_idx != n_attrs - 1:
+                    llm_input += ", "
+            llm_input_vector.append(llm_input)
+        
+        res = generator(llm_input_vector, max_new_tokens=30, do_sample=False)
+        pa_arr = pa.array([res[i]['generated_text'] for i in range(len(attributes[0]))])
         return pa_arr
     else:
         print(task, 'is unsupported')
